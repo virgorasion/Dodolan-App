d2498022a364a2ebbc464f246a00bf9d3cb48a6b
M Nur Fauzan W <fauzan.widyanto1@gmail.com>
2020-08-19 08:42:30 +0700

    update

--- app/src/main/java/com/fazahidah/dodolan/LoginActivity.kt
+++ app/src/main/java/com/fazahidah/dodolan/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.fazahidah.dodolan
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity: AppCompatActivity() {
@@ -8,5 +10,11 @@ class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_page)
+
+        val login = findViewById<Button>(R.id.button_login)
+        login.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
